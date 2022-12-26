6a205c34f0e7201892fdb81a398518a051f37c4c
OlgaJlga <119102783+OlgaJlga@users.noreply.github.com>
2022-12-26 17:37:43 +0300

    Add files via upload

---
+++ Program66.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите M :");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N :");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма элементов от {M} до {N} = {NaturalSum(M, N)}");
+int NaturalSum(int M, int N)
+{
+    if (M == N)
+        return N;
+    return N + NaturalSum(M, N - 1);
+}
---
+++ Program68.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.WriteLine("Введите m :");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n :");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1 && m < 1)
+{
+ Console.WriteLine("Ввели не положительное число");
+}
+else
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+int Akkerman(int m, int n)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akkerman(n - 1, 1);
+    else
+      return Akkerman(n - 1, Akkerman(n, m - 1));
+}

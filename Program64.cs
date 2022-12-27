c096c41fb20544f0a3538ade82cc432bab941f8d
OlgaJlga <119102783+OlgaJlga@users.noreply.github.com>
2022-12-27 16:46:15 +0300

    Add files via upload

---
+++ Program64.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int o = 1;
+if (N < 1)
+{
+ Console.WriteLine("Ввели не положительное число");
+}
+else
+Console.WriteLine(NaturalNumber(N, o));
+int NaturalNumber(int N, int o)
+{
+ if (N == o)
+ return N;
+ else
+ Console.Write($"{NaturalNumber(N, o + 1)}, ");
+ return o;
+}
